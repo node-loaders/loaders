5ea01aa4870a1fc07156456ea5bcc85da698e529
Marcelo Shima <marceloshima@gmail.com>
2022-12-01 21:12:09 -0300

    fix(mock): fix resolveCallerUrl when stack-utils returns relative path

--- workspaces/mock/src/caller-resolve.ts
+++ workspaces/mock/src/caller-resolve.ts
@@ -1,4 +1,4 @@
-import { isAbsolute } from 'node:path';
+import { isAbsolute, join } from 'node:path';
 import { pathToFileURL } from 'node:url';
 import StackUtils from 'stack-utils';
 
@@ -6,6 +6,12 @@ export const resolveCallerUrl = (): string => {
   const stack = new StackUtils();
   const error = new Error('Get stack');
   const lines = error.stack?.split('\n');
+  if (lines) {
+    while (lines.length > 0 && !lines[0].startsWith('Error: Get stack')) {
+      lines.shift();
+    }
+  }
+
   if (lines && lines.length > 3) {
     const parsed = stack.parseLine(lines[3]);
     if (parsed?.file) {
@@ -13,7 +19,13 @@ export const resolveCallerUrl = (): string => {
         return pathToFileURL(parsed.file).href;
       }
 
-      return parsed.file;
+      try {
+        new URL(parsed.file);
+        return parsed.file;
+      } catch {}
+
+      // ParseLine makes the file relative.
+      return pathToFileURL(join(process.cwd(), parsed.file)).href;
     }
   }
 
