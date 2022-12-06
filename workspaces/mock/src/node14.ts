4aeb504f5a9574960a9b5fcdf4aca131e3e69e91
Marcelo Shima <marceloshima@gmail.com>
2022-12-05 23:52:30 -0300

    fix(mock): fix _getFormat implementation

--- workspaces/mock/src/node14.ts
+++ workspaces/mock/src/node14.ts
@@ -1,9 +1,33 @@
-import { addNode14Support } from '@node-loaders/core';
+import { addNode14Support, isBuiltinModule } from '@node-loaders/core';
 import MockLoader from './mock-loader.js';
+import { type MockedSpecifierData, parseProtocol } from './url-protocol.js';
+import { addMockedSpecifier, getMockedData } from './module-cache.js';
 
 export class Node14MockLoader extends addNode14Support(MockLoader) {
-  async _getFormat(url: string, context: Record<string, unknown>): Promise<{ format: string } | undefined> {
-    return { format: 'module' };
+  async _getFormat(
+    url: string,
+    context: Record<string, unknown>,
+    defaultGetFormat: (url: string, context: Record<string, unknown>) => Promise<{ format: string }>,
+  ): Promise<{ format: string } | undefined> {
+    const mockData = parseProtocol(url) as MockedSpecifierData;
+    if (mockData) {
+      if (isBuiltinModule(mockData.specifier)) {
+        if (!getMockedData(mockData.cacheId, mockData.specifier)) {
+          // At node 14 we always need to mock a builtin module
+          addMockedSpecifier(mockData.cacheId, mockData.specifier, {});
+        }
+
+        return { format: 'module' };
+      }
+
+      if (getMockedData(mockData.cacheId, mockData.specifier)) {
+        return { format: 'module' };
+      }
+
+      return defaultGetFormat(mockData.resolvedSpecifier, context);
+    }
+
+    return defaultGetFormat(url, context);
   }
 }
 
@@ -19,4 +43,3 @@ export const load = loader.exportLoad();
 // Keep node 14 compatibility
 export const getFormat = loader.exportGetFormat();
 export const getSource = loader.exportGetSource();
-export const transformSource = loader.exportTransformSource();
