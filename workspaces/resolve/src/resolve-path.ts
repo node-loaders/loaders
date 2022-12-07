cba4f26baa7cf73d9b732ea273b559527f22a815
Marcelo Shima <marceloshima@gmail.com>
2022-12-07 00:38:48 -0300

    feat(resolve): add sync methods

--- workspaces/resolve/src/resolve-path.ts
+++ workspaces/resolve/src/resolve-path.ts
@@ -1,9 +1,10 @@
 import { stat as fsStat } from 'node:fs/promises';
+import { statSync } from 'node:fs';
 import { dirname, extname, isAbsolute, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-import { locatePath } from 'locate-path';
-import { readPackageUp } from 'read-pkg-up';
+import { locatePath, locatePathSync } from 'locate-path';
+import { readPackageUp, readPackageUpSync } from 'read-pkg-up';
 
 /**
  * Look for nearest package.json type field
@@ -12,6 +13,16 @@ import { readPackageUp } from 'read-pkg-up';
  */
 export async function detectPackageJsonType(filePath: string): Promise<'commonjs' | 'module'> {
   const read = await readPackageUp({ cwd: dirname(filePath) });
+  return read!.packageJson.type ?? 'commonjs';
+}
+
+/**
+ * Look for nearest package.json type field
+ * @param filePath
+ * @returns
+ */
+export function detectPackageJsonTypeSync(filePath: string): 'commonjs' | 'module' {
+  const read = readPackageUpSync({ cwd: dirname(filePath) });
   return read!.packageJson.type ?? 'commonjs';
 }
 
@@ -52,6 +63,20 @@ export const existingFile = async (filePath: string): Promise<string | undefined
 };
 
 /**
+ * Check if the file exists
+ * @param filePath
+ * @returns the filePath if is a file and exists, undefined otherwise
+ */
+export const existingFileSync = (filePath: string): string | undefined => {
+  try {
+    const stat = statSync(filePath);
+    return stat.isFile() ? filePath : undefined;
+  } catch {}
+
+  return undefined;
+};
+
+/**
  * Look for file modules with the extension or index with extension if the passed filePath is a directory
  * @param filePath
  * @param extension
@@ -62,6 +87,16 @@ export const lookForDefaultModule = async (filePath: string, extension = '.js'):
 };
 
 /**
+ * Look for file modules with the extension or index with extension if the passed filePath is a directory
+ * @param filePath
+ * @param extension
+ * @returns the default filePath fallback if is found, undefined otherwise
+ */
+export const lookForDefaultModuleSync = (filePath: string, extension = '.js'): string | undefined => {
+  return locatePathSync([join(filePath, `${'index'}${extension}`), `${filePath}${extension}`], { type: 'file' });
+};
+
+/**
  * Look for the filePath with alternative extensions
  * @param filePath
  * @returns existing files with alternative extensions
@@ -69,8 +104,16 @@ export const lookForDefaultModule = async (filePath: string, extension = '.js'):
 export const resolveAlternativeFile = async (filePath: string, extensions: string[]): Promise<string | undefined> => {
   const extension = extname(filePath);
   const extensionLess = filePath.slice(0, -extension.length);
-  return locatePath(
-    extensions.map(alternative => `${extensionLess}${alternative}`),
-    { type: 'file' },
-  );
+  return locatePath([filePath, ...extensions.map(alternative => `${extensionLess}${alternative}`)], { type: 'file' });
+};
+
+/**
+ * Look for the filePath with alternative extensions
+ * @param filePath
+ * @returns existing files with alternative extensions
+ */
+export const resolveAlternativeFileSync = (filePath: string, extensions: string[]): string | undefined => {
+  const extension = extname(filePath);
+  const extensionLess = filePath.slice(0, -extension.length);
+  return locatePathSync([filePath, ...extensions.map(alternative => `${extensionLess}${alternative}`)], { type: 'file' });
 };
--- workspaces/resolve/test/resolve-path.spec.ts
+++ workspaces/resolve/test/resolve-path.spec.ts
@@ -8,6 +8,10 @@ import {
   resolveAlternativeFile,
   lookForDefaultModule,
   specifierToFilePath,
+  lookForDefaultModuleSync,
+  resolveAlternativeFileSync,
+  detectPackageJsonTypeSync,
+  existingFileSync,
 } from '../src/resolve-path.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -29,6 +33,21 @@ describe('resolve-module', () => {
     });
   });
 
+  describe('existingFileSync', () => {
+    it('should return true for existing file', () => {
+      const filePath = join(__dirname, 'fixtures/foo.txt');
+      expect(existingFileSync(filePath)).toBe(filePath);
+    });
+    it('should return undefined for non existing file', () => {
+      const filePath = join(__dirname, 'fixtures/bar.txt');
+      expect(existingFileSync(filePath)).toBe(undefined);
+    });
+    it('should return undefined for directory', () => {
+      const filePath = join(__dirname, 'fixtures');
+      expect(existingFileSync(filePath)).toBe(undefined);
+    });
+  });
+
   describe('detectFormatForEsbuildFilePath', () => {
     it('should return commonjs when nearest package.json type field is undefined', async () => {
       const module = join(__dirname, 'fixtures/detect-format/commonjs/module.cts');
@@ -40,10 +59,29 @@ describe('resolve-module', () => {
     });
   });
 
+  describe('detectFormatForEsbuildFilePathSync', () => {
+    it('should return commonjs when nearest package.json type field is undefined', () => {
+      const module = join(__dirname, 'fixtures/detect-format/commonjs/module.cts');
+      expect(detectPackageJsonTypeSync(module)).toBe('commonjs');
+    });
+    it('should return the nearest package.json type field', () => {
+      const module = join(__dirname, 'fixtures/detect-format/esm/module.mts');
+      expect(detectPackageJsonTypeSync(module)).toBe('module');
+    });
+  });
+
   describe('resolveAlternativeFile', () => {
     it('should return alternative extension for file', async () => {
       const module = join(__dirname, 'fixtures/alternative-files/module.js');
       const alternativeFile = await resolveAlternativeFile(module, ['.unknown', '.bar', '.foo']);
+      expect(alternativeFile).toEqual(join(__dirname, 'fixtures/alternative-files/module.bar'));
+    });
+  });
+
+  describe('resolveAlternativeFileSync', () => {
+    it('should return alternative extension for file', () => {
+      const module = join(__dirname, 'fixtures/alternative-files/module.js');
+      const alternativeFile = resolveAlternativeFileSync(module, ['.unknown', '.bar', '.foo']);
       expect(alternativeFile).toEqual(join(__dirname, 'fixtures/alternative-files/module.bar'));
     });
   });
@@ -56,6 +94,17 @@ describe('resolve-module', () => {
     it('should add index.js to the directory', async () => {
       const module = join(__dirname, 'fixtures/default-modules');
       expect(await lookForDefaultModule(module)).toBe(resolve(`${module}/index.js`));
+    });
+  });
+
+  describe('lookForDefaultModuleSync', () => {
+    it('should add extension to the non existing file', () => {
+      const module = join(__dirname, 'fixtures/default-modules/index');
+      expect(lookForDefaultModuleSync(module)).toBe(`${module}.js`);
+    });
+    it('should add index.js to the directory', () => {
+      const module = join(__dirname, 'fixtures/default-modules');
+      expect(lookForDefaultModuleSync(module)).toBe(resolve(`${module}/index.js`));
     });
   });
 
