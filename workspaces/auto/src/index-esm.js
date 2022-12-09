089ed8ec1369caa0e150bb017b26033f1500db9b
Marcelo Shima <marceloshima@gmail.com>
2022-12-08 21:14:24 -0300

    feat(auto): add strict and esm exports

---
+++ workspaces/auto/src/index-esm.js
@@ -0,0 +1,19 @@
+import { ChainLoader } from '@node-loaders/core';
+
+let mock;
+let esbuild;
+
+try {
+  mock = await import('@node-loaders/mock');
+} catch {}
+
+try {
+  // eslint-disable-next-line n/file-extension-in-import
+  esbuild = await import('@node-loaders/esbuild/esm');
+} catch {}
+
+const loader = new ChainLoader([mock, esbuild].filter(Boolean));
+
+export const resolve = loader.exportResolve();
+
+export const load = loader.exportLoad();
