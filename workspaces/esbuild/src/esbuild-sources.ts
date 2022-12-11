956a95bd50c0918005a735d4090939ec9ec25e74
Marcelo Shima <marceloshima@gmail.com>
2022-12-10 22:53:15 -0300

    fix(esbuild): fix cache extension for imported cjs. add 2 cache modifiers.

--- workspaces/esbuild/src/esbuild-sources.ts
+++ workspaces/esbuild/src/esbuild-sources.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'node:url';
 import { getTsconfig } from 'get-tsconfig';
 import { type Format } from '@node-loaders/core';
 import { detectPackageJsonType, LoaderCache } from '@node-loaders/resolve';
-import { transform, type TransformOptions, transformSync } from 'esbuild';
+import { transform, type TransformOptions, transformSync, type Format as EsbuildFormat } from 'esbuild';
 
 type EsbuildSource = {
   source: string;
@@ -18,13 +18,28 @@ type TsconfigCache = {
   hash: string;
 };
 
-const cacheExtension = 'mjs';
+const getEsbuildFormatForFormat = (format: Format): EsbuildFormat => (format === 'module' ? 'esm' : 'cjs');
+const getExtensionForFormat = (format: Format): string => (format === 'module' ? 'mjs' : 'cjs');
+
+const defaultEsbuildOptions: TransformOptions = {
+  loader: 'default',
+  minifyWhitespace: true,
+  keepNames: false,
+  sourcemap: 'inline',
+};
 
 export class EsbuildSources {
+  readonly esbuildOptions: TransformOptions;
   readonly sourcesCache: Record<string, EsbuildSource> = {};
   readonly tsconfigCache: Record<string, TsconfigCache> = {};
   readonly cache: LoaderCache = new LoaderCache('esbuild');
+  readonly optionsHash: string;
   private sourceMapSet = false;
+
+  constructor(esbuildOptions = defaultEsbuildOptions) {
+    this.esbuildOptions = esbuildOptions;
+    this.optionsHash = this.cache.createHash(JSON.stringify(defaultEsbuildOptions));
+  }
 
   setupSourceMap() {
     if (this.sourceMapSet) {
@@ -40,7 +55,7 @@ export class EsbuildSources {
   }
 
   /* c8 ignore next */
-  async getSource(fileUrl: string, maybeFormat?: string): Promise<EsbuildSource> {
+  async getSource(fileUrl: string, maybeFormat?: Format): Promise<EsbuildSource> {
     if (this.sourcesCache[fileUrl]) {
       return this.sourcesCache[fileUrl];
     }
@@ -52,21 +67,21 @@ export class EsbuildSources {
     const fileContent = sourceFile.toString();
 
     const tsconfigCache = this.findTsConfig(filePath);
-    const cacheOptions = { modifier: tsconfigCache?.hash, extension: cacheExtension };
-    let transformed = await this.cache.get(fileContent, cacheOptions);
 
     /* c8 ignore next 2 */
-    const format: Format = (maybeFormat ?? (await detectPackageJsonType(filePath)) ?? 'module') as Format;
-    const esbuildFormat = format === 'module' ? 'esm' : 'cjs';
+    const format: Format = maybeFormat ?? (await detectPackageJsonType(filePath)) ?? 'module';
+    const esbuildFormat = getEsbuildFormatForFormat(format);
 
+    const cacheOptions = { modifiers: [this.optionsHash, tsconfigCache?.hash], extension: getExtensionForFormat(format) };
+    let transformed = await this.cache.get(fileContent, cacheOptions);
     if (!transformed) {
-      const result = await transform(
-        fileContent,
-        this.getOptions(tsconfigCache?.config, {
-          sourcefile: filePath,
-          format: esbuildFormat,
-        }),
-      );
+      const result = await transform(fileContent, {
+        ...this.esbuildOptions,
+        sourcefile: filePath,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        tsconfigRaw: tsconfigCache?.config,
+        format: getEsbuildFormatForFormat(format),
+      });
       transformed = result.code;
       await this.cache.save(transformed, cacheOptions);
     }
@@ -77,10 +92,13 @@ export class EsbuildSources {
   }
 
   /* c8 ignore next */
-  getSourceSync(fileUrl: string, format: Format = 'commonjs'): EsbuildSource {
+  getSourceSync(fileUrl: string): EsbuildSource {
     if (this.sourcesCache[fileUrl]) {
       return this.sourcesCache[fileUrl];
     }
+
+    // Sync method supports commonjs only
+    const commonjsFormat: Format = 'commonjs';
 
     this.setupSourceMap();
 
@@ -88,35 +106,27 @@ export class EsbuildSources {
 
     const fileContent = readFileSync(filePath, 'utf8').toString();
     const tsconfigCache = this.findTsConfig(filePath);
-    const cacheOptions = { file: this.cache.createHash(fileContent), modifier: tsconfigCache?.hash, extension: cacheExtension };
+    const cacheOptions = {
+      file: this.cache.createHash(fileContent),
+      modifiers: [this.optionsHash, tsconfigCache?.hash],
+      extension: getExtensionForFormat(commonjsFormat),
+    };
 
     let transformed = this.cache.getSync(cacheOptions);
     if (!transformed) {
-      const result = transformSync(
-        fileContent,
-        this.getOptions(tsconfigCache?.config, {
-          sourcefile: filePath,
-          format: 'cjs',
-        }),
-      );
+      const result = transformSync(fileContent, {
+        ...this.esbuildOptions,
+        sourcefile: filePath,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        tsconfigRaw: tsconfigCache?.config,
+        format: getEsbuildFormatForFormat(commonjsFormat),
+      });
       transformed = result.code;
       this.cache.saveSync(transformed, cacheOptions);
     }
 
-    this.sourcesCache[fileUrl] = { source: transformed, format };
+    this.sourcesCache[fileUrl] = { source: transformed, format: commonjsFormat };
     return this.sourcesCache[fileUrl];
-  }
-
-  getOptions(tsconfig: any, options: TransformOptions & Required<Pick<TransformOptions, 'sourcefile'>>): TransformOptions {
-    return {
-      loader: 'default',
-      minifyWhitespace: true,
-      keepNames: true,
-      sourcemap: 'inline',
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      tsconfigRaw: tsconfig,
-      ...options,
-    };
   }
 
   findTsConfig(sourceFile: string): TsconfigCache {
